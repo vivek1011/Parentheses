fa13fb34e15ce6bad962b468535d14a6cb6ac286
Vivek Bansal <bansalvivek79@gmail.com>
2016-12-10 00:59:53 +0530

    Create brackets.cpp

---
+++ brackets.cpp
@@ -0,0 +1,10 @@
+http://codeforces.com/problemset/gymProblem/100803/G - Flipping parentheses .
+Logic : If you turn ( -> ) , then first occuring ) should be turned to ( . 
+But if ) -> ( then find one minimum position so that minimum balance in range [finalposition , changedposition] >= 2 
+So that if i change the balance at the finalposition then balance in this range must be greater than equal to 0. 
+So that string must be balanced in this segment and hence overall segment . 
+
+Find longest balanced brackets subsequence in a range . 
+http://codeforces.com/problemset/problem/380/C 
+
+
